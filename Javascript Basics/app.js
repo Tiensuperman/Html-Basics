87f5eb7e4ac65d048751b5334f617a44adf3a634
Tiensuperman <ddtien@outlook.com>
2022-09-05 16:39:53 +0700

    Javascript Basics

---
+++ Javascript Basics/app.js
@@ -0,0 +1,43 @@
+//let cash = 50;
+//let price = 40;
+//let count = 1;
+//let isStoreOpen = true;
+
+//let str = cash >= price && isStoreOpen ? "Give Receipt" : "Do Not Give Receipt"
+//console.log (str)
+
+//while (count <= 10) {
+    //console.log (count)
+    //count = count + 1
+//}
+
+//for (let i = 1; i <= 20; ++i) {
+    //if (i % 3 === 0) {
+        //console.log(`${i} -> Frontend`)
+    //}
+    //else if (i % 5 === 0) {
+        //console.log(`${i} -> Simplified`)
+    //}
+    //else if (i % 3 === 0 && i % 5 === 0) {
+        //console.log(`${i} -> Frontend Simplified`)
+    //}
+    //else {
+        //console.log(`${i} -> ${i}`)
+    //}
+//}
+
+    //const str = "Frontend Simplified"
+
+    //for (let i = 0; i < str.length; ++1) {
+        //console.log (str[i])
+    //}
+
+    //Function Defenition
+    function welcomePersonToFES (name) {
+        console.log (`Welcome to Frontend Simplified, ${name}`)
+    }
+
+    //Call The Function
+    welcomePersonToFES ('Mitri');
+    welcomePersonToFES ('Tien');
+    welcomePersonToFES ('Jimmy');
